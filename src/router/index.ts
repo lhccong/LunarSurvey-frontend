e29afff2580dfdd01293ff0a15b10a08a4d134b9
lhccong <liuhuaicong@zhut.cn>
2024-05-22 18:14:44 +0800

    feat: 🚀新增动态读取路由 js 文件形式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,5 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomePage from "@/views/HomePage.vue";
-
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "home",
-    component: HomePage,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-];
+import { createRouter, createWebHistory } from "vue-router";
+import { routes } from "@/router/routes";
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
