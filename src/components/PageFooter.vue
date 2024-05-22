ba0ad397b41d03ac930531485a953fe98732c339
lhccong <liuhuaicong@zhut.cn>
2024-05-22 17:57:38 +0800

    feat: 🚀新增抽象组件

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts"></script>
+
+<template>
+  <a-space>
+    <a-link
+      style="color: #848587; margin-right: 25px"
+      href="https://github.com/lhccong"
+    >
+      <template #icon>
+        <icon-github style="font-size: 20px" />
+      </template>
+      站长：程序员聪
+    </a-link>
+    <a-link
+      style="color: #848587; margin-right: 25px"
+      href="https://github.com/lhccong"
+    >
+      <template #icon>
+        <icon-exclamation-circle-fill style="font-size: 20px" />
+      </template>
+      关于本站
+    </a-link>
+    <a-link style="color: #848587" href=" https://github.com/lhccong">
+      <template #icon>
+        <icon-bug style="font-size: 20px" />
+      </template>
+      建议反馈
+    </a-link>
+  </a-space>
+</template>
+
+<style scoped></style>
