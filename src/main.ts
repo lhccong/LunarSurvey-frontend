45c31ef5101531fec9f09323a702b8b50a208ce0
lhccong <liuhuaicong@zhut.cn>
2024-05-23 16:20:09 +0800

    feat: 新增全局页面用户权限校验

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import router from "./router";
 import { createPinia } from "pinia";
 import ArcoVue from "@arco-design/web-vue";
 import "@arco-design/web-vue/dist/arco.css";
+import "@/access";
+
 // 额外引入图标库
 import ArcoVueIcon from "@arco-design/web-vue/es/icon";
 
